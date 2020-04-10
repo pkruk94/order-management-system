fb65df825e14597610d9464a392edad4feb6ba1b
Pawel <pkruk94@gmail.com>
2020-04-10 19:40:27 +0200

    Added CrudRepository interface

---
+++ infrastructure/src/main/java/repository/generic/CrudRepository.java
@@ -0,0 +1,16 @@
+package repository.generic;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudRepository<T, ID> {
+
+    Optional<T> addOrUpdate(T t);
+    List<T> addOrUpdateMany(List<T> items);
+    Optional<T> findByID(ID id);
+    List<T> findAllByID(List<ID> ids);
+    List<T> findAll();
+    Optional<T> deleteByID(ID id);
+    List<T> deleteAllByID(List<ID> ids);
+    boolean deleteAll();
+}
