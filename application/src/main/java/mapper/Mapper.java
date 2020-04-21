e68f475f27bda41ec4d1e64e1eb5b7499022ee7f
Pawel <pkruk94@gmail.com>
2020-04-21 22:50:26 +0200

    Implemented mapper Mapper.fromCreateProductDTOtoProductEntity

--- application/src/main/java/mapper/Mapper.java
+++ application/src/main/java/mapper/Mapper.java
@@ -1,10 +1,24 @@
 package mapper;
 
 import dto.CreateProduct;
+import offered_commodity.OfferedCommodity;
+import order_position.OrderPosition;
+import producer.Producer;
 import product.Product;
+import product.ProductCategory;
+import value_object.Money;
+import warehouse_commodity.WarehouseCommodity;
+import warranty.Warranty;
+
+import javax.persistence.*;
+import java.util.List;
 
 public interface Mapper {
     static Product fromCreateProductDTOtoProductEntity(CreateProduct createProduct) {
-        return null;
+        return Product.builder()
+                .name(createProduct.getName())
+                .price(createProduct.getPrice())
+                .productCategory(createProduct.getProductCategory())
+                .build();
     }
 }
